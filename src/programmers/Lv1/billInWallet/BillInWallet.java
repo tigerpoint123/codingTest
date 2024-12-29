36c417e8ee4dce6a01e1e9bf9b476b8b74ad1ac9
tigerpoint123 <tigerrla@naver.com>
2024-12-29 22:48:38 +0900

    bill in wallet. no exception

--- src/programmers/Lv1/billInWallet/BillInWallet.java
+++ src/programmers/Lv1/billInWallet/BillInWallet.java
@@ -2,23 +2,36 @@ package programmers.Lv1.billInWallet;
 
 public class BillInWallet {
     public static void main(String[] args) {
-        int[] wallet = {50, 50};
-        int[] bill = {100, 241};
+        int[] wallet = {30, 15};
+        int[] bill = {26, 17};
         int answer = 0;
         while(true) {
+            int tempW = 0;
+            int tempB = 0;
+            if(wallet[0] < wallet[1]){
+                tempW = wallet[0];
+                wallet[0] = wallet[1];
+                wallet[1] = tempW;
+            }
+            if(bill[0] < bill[1]){
+                tempB = bill[0];
+                bill[0] = bill[1];
+                bill[1] = tempB;
+            }
+
+            if(bill[0] <= wallet[0] && bill[1] <= wallet[1]) break;
+
             if(bill[0] > bill[1]) {
                 bill[0] = bill[0]/2;
                 answer ++;
-            } else {
+            } else if(bill[0] < bill[1]) {
                 bill[1] = bill[1] / 2;
+                answer ++;
+            } else {
+                bill[0] = bill[0]/2;
                 answer ++;
             }
 
-
-            if((bill[0] <= wallet[0] || bill[0] <= wallet[1])
-                    && (bill[1] <= wallet[0] || bill[1] <= wallet[1])) {
-                break;
-            }
         }
 
         System.out.println(answer);
